ce3c6539c458add7d41fb94323b7564bc2d5be33
DVR <age.star@mail.ru>
2023-08-11 00:23:18 +0300

    solved tasks 2,3,4,5

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>() {{
+        List<Integer> listInt = new ArrayList<>() {{
             add(1);
             add(2);
             add(3);
@@ -15,7 +16,31 @@ public class Main {
             add(6);
         }};
 
-        int result = list.stream().mapToInt(Integer::intValue).sum();
+        List<String> listString = new ArrayList<>() {{
+            add("One");
+            add("Two");
+            add("Three");
+            add("Four");
+        }};
+
+        // 1. Напишите программу, которая находит сумму всех чисел в списке, используя Stream API.
+        int result = listInt.stream().mapToInt(Integer::intValue).sum();
         System.out.println(result);
+
+        //2. Напишите программу, которая находит самое длинное слово в списке строк с использованием Stream API.
+        String str = listString.stream().max((s1, s2) -> s1.length() - s2.length()).orElse("");
+        System.out.println(str);
+
+        //3. Напишите программу, которая фильтрует список строк и оставляет только строки, начинающиеся с определенной буквы, используя Stream API.
+        List<String> listOf_T_Words = listString.stream().filter(x -> x.startsWith("T")).toList();
+        System.out.println(listOf_T_Words);
+
+        //4. Напишите программу, которая преобразует список чисел в список их квадратов, используя Stream API.
+        List<Integer> squareNumber = listInt.stream().mapToInt(x -> x * x).boxed().toList();
+        System.out.println(squareNumber);
+
+        //5. Напишите программу, которая находит среднее значение списка чисел с использованием Stream API.
+        double average = listInt.stream().mapToInt(Integer :: intValue).average().getAsDouble();
+        System.out.println(average);
     }
 }
